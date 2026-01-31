c60739058f4a58fa11e9cdfb235690177b6a3f7b
elianf-dev <efigueroa2@my.dom.edu>
2026-01-31 00:45:23 -0600

    Added BLL and DAL. As well as the navigation, and a working log in, register and log out. Local Db works as well.

---
+++ TheCollectorsKeepGitFolder/DataAccessLayer/DataModels/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataModels
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string QuantityAvailable { get; set; }
+
+        //Still missing add images and condition
+
+    }
+}
---
+++ TheCollectorsKeepGitFolder/DataAccessLayer/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.DataModels;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Data;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly CollectorsKeepDbContext _context;
+
+        public ProductRepository(CollectorsKeepDbContext context)
+        {
+            _context = context;
+        }
+        public List<Product> GetAll()
+    => _context.Products.ToList();
+
+        public Product? GetById(int id)
+            => _context.Products.Find(id);
+
+        public void Add(Product product)
+            => _context.Products.Add(product);
+
+        public void Update(Product product)
+            => _context.Products.Update(product);
+
+        public void Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+                _context.Products.Remove(product);
+        }
+
+        public void Save()
+            => _context.SaveChanges();
+
+    }
+}
