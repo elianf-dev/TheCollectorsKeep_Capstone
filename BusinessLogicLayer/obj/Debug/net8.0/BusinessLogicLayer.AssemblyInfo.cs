8cd5cc71865fab5513db6363a6221eaf88312337
Juan Pantoja <87252828+ToledoCat@users.noreply.github.com>
2026-02-11 14:12:56 -0600

    Added the Products page and 3 examples
    Fixed issues with duplicate assemblies. Created a products page with three products per row for now. Added three example products.

--- BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1919307965411fd3cec06d19b6c1f6c8a940b6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6c67a06f7122cf95d2f17ddf3328a255f7eac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TheCollectorsKeepGitFolder/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Data;
+using DataAccessLayer.DataModels;
+
+public class ProductsController : Controller
+{
+    private readonly CollectorsKeepDbContext _context;
+
+    public ProductsController(CollectorsKeepDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var products = await _context.Products.ToListAsync();
+        return View(products);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var product = await _context.Products
+            .FirstOrDefaultAsync(p => p.ProductID == id);
+
+        if (product == null)
+            return NotFound();
+
+        return View(product);
+    }
+}
